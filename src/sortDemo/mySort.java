a140719ee703fb41654b91a55999da68e99c539b
3ilence <2607840393@qq.com>
2022-03-05 16:04:24 +0800

    refactor

---
+++ src/sortDemo/mySort.java
@@ -0,0 +1,66 @@
+package sortDemo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class mySort {
+    public abstract class Sort<T extends Comparable<T>> {
+
+        public abstract void sort(T[] nums);
+
+        protected boolean less(T v, T w) {
+            return v.compareTo(w) < 0;
+        }
+
+        protected void swap(T[] a, int i, int j) {
+            T t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+    }
+
+    public class QuickSort<T extends Comparable<T>> extends Sort<T> {
+        @Override
+        public void sort(T[] nums) {
+            shuffle(nums);
+            sort(nums,0 ,nums.length - 1);
+        }
+
+        private void sort(T[] nums,int l, int h) {
+            if(h <= l)
+                return;
+            int j = partition(nums,l,h);
+            sort(nums,l,j - 1);
+            sort(nums,j + 1,h);
+        }
+
+        private void shuffle(T[] nums) {
+            List<Comparable> list = Arrays.asList(nums);
+            Collections.shuffle(list);
+            list.toArray(nums);
+        }
+
+        private int partition(T[] nums,int l,int h) {
+            int i = l,j = h + 1;
+            T v = nums[l];
+            while (true) {
+                while (less(nums[++i],v) && i != h);
+                while (less(v,nums[--j]) && j != l);
+                if(i >= j)
+                    break;
+                swap(nums,i,j);
+            }
+            swap(nums,l,j);
+            return j;
+        }
+
+        public void swap(T[] nums, int i, int j) {
+            T tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+
+
+}
