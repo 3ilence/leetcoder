ec7b194183979aff7d4cb7d7c515ac3b6288f978
3ilence <2607840393@qq.com>
2022-03-21 19:56:14 +0800

    1035. 不相交的线。动规，这一题乍一看没有思路，但是其实本身就是最长公共子序列，关键是如何对表面问题进行归纳，找到到底我们要做什么。

---
+++ src/DynamicProgramming/MaxUncrossedLines.java
@@ -0,0 +1,33 @@
+package DynamicProgramming;
+
+/**
+ * @ClassName : MaxUncrossedLines
+ * @Author : Silence
+ * @Date: 2022/3/21 19:53
+ * @Description : 1035. 不相交的线
+ */
+public class MaxUncrossedLines {
+
+    // <https://leetcode-cn.com/problems/uncrossed-lines/>
+    /**
+     * 其实就是最长公共子序列
+     * @param nums1 nums1
+     * @param nums2 nums2
+     * @return res
+     */
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        //判断线相不相交，下标差的乘积如果是正就相交
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
