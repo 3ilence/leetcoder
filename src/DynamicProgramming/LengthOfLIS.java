408947b6f0f6712aac91decbe66622cff1c0b4c8
3ilence <2607840393@qq.com>
2022-03-21 16:11:12 +0800

    300. 最长递增子序列。动规，经典题目没啥好说的。

---
+++ src/DynamicProgramming/LengthOfLIS.java
@@ -0,0 +1,35 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName : LengthOfLIS
+ * @Author : Silence
+ * @Date: 2022/3/21 16:09
+ * @Description : 300. 最长递增子序列
+ */
+public class LengthOfLIS {
+
+    /**
+     * 动态规划
+     * @param nums nums
+     * @return res
+     */
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];//dp[i]以i结尾的最长子序列
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++ ) {
+                if (dp[i] >= dp[j] + 1) {
+                    continue;
+                } else if (nums[i] > nums[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            res = Math.max(dp[i], res);
+        }
+        return res;
+    }
+}
