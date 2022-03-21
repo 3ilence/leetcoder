352e1283021c34d8aa4617dd6a9dded02d1e75e8
3ilence <2607840393@qq.com>
2022-03-21 14:10:55 +0800

    122&123&188&309. 买卖股票的最佳时机。动规，关键是前两题，一个是买卖一次，另一个买卖两次。关键是第二题的那种做法，很巧妙。后面两题都非常类似，股票问题定义好状态数组都很简单。

--- src/DynamicProgramming/MaxProfit.java
+++ src/DynamicProgramming/MaxProfit.java
@@ -4,7 +4,7 @@ package DynamicProgramming;
  * @ClassName : MaxProfit
  * @Author : Silence
  * @Date: 2022/3/5 9:00
- * @Description : 122&123. 买卖股票的最佳时机
+ * @Description : 122&123&188&309. 买卖股票的最佳时机
  */
 public class MaxProfit {
 
@@ -30,7 +30,7 @@ public class MaxProfit {
 
     // <https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/>
     /**
-     * 123. 买卖股票的最佳时机 III
+     * 123. 买卖股票的最佳时机 III。其实这种做法可以优化，类似与188买卖股票4的做法
      * @param prices 价格
      * @return 最大利润
      */
@@ -84,7 +84,74 @@ public class MaxProfit {
         return sell2;
     }
 
+    // <https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/>
+    /**
+     * 188. 买卖股票的最佳时机 IV。解法与123十分相似，就是不能空间优化
+     * @param k 限制次数
+     * @param prices 价格
+     * @return 最大利润
+     */
+    public int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        int[][][] dp = new int[len][2][k+1];//dp[i][j][k]代表第i天持有股票股票还剩k次卖的机会
+        for (int i = 0; i <= k; i++) {
+            dp[0][1][i] = -prices[0];//假装我在第0天的时候已经卖过股票，这样的好处是免去了分类讨论，并且使得dp[len-1][0][0]就是最终答案，因为不管利润最大的时候买了几次，都会到达dp[len-1][0][0]
+        }
+        for (int i = 1; i < len; i++) {
+            for (int j = k; j >= 0; j--) {
+                if (j < k) {
+                    dp[i][0][j] = Math.max(dp[i-1][0][j], dp[i-1][1][j+1] + prices[i]);
+                } else {
+                    dp[i][0][j] = dp[i-1][0][j];
+                }
+                dp[i][1][j] = Math.max(dp[i-1][1][j], dp[i-1][0][j] - prices[i]);
+            }
+        }
+        return dp[len-1][0][0];
+    }
+
+    // <https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/>
+    /**
+     * 309. 最佳买卖股票时机含冷冻期。空间未优化版本。
+     * @param prices 价格
+     * @return 最大利润
+     */
+    public int maxProfit5(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int len = prices.length;
+        int[][][] dp = new int[len][2][2];//i,j,k,k=1表示处于冷冻期
+        dp[0][1][0] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0][0] = Math.max(dp[i-1][0][0], dp[i-1][0][1]);
+            dp[i][0][1] = dp[i-1][1][0] + prices[i];//持有股票的时候不可能处于冷冻期
+            dp[i][1][0] = Math.max(dp[i-1][1][0], dp[i-1][0][0] - prices[i]);
+        }
+        return Math.max(dp[len-1][0][0], dp[len-1][0][1]);
+    }
+
+    /**
+     * 309. 最佳买卖股票时机含冷冻期。空间优化版本。
+     * @param prices 价格
+     * @return 最大利润
+     */
+    public int maxProfit6(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int len = prices.length;
+        int a = -prices[0], b = 0, c = 0;//有票，无票且冷，无票不冷
+        for (int i = 1; i < len; i++) {
+            int tmp = c;
+            c = Math.max(c, b);
+            b = a + prices[i];//持有股票的时候不可能处于冷冻期
+            a = Math.max(a, tmp - prices[i]);
+        }
+        return Math.max(b, c);
+    }
+
     public static void main(String[] args) {
-        System.out.println(new MaxProfit().maxProfit3(new int[] {1,4,2,7}));
+        System.out.println(new MaxProfit().maxProfit(2, new int[] {1,4,2,7}));
     }
 }
