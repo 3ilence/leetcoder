3458e6f4d2523c49343b2565eae56be88417d2e4
3ilence <silence.zhu.cn@gmail.com>
2022-11-24 16:59:06 +0800

    64.minPathSum: DP, space complexity O(m*n) -> O(m)

---
+++ src/DynamicProgramming/MinPathSum.java
@@ -0,0 +1,63 @@
+package DynamicProgramming;
+
+/**
+ * @ClassName : MinPathSum
+ * @Author : Silence
+ * @Date: 2022/11/24 16:54
+ * @Description : 64. 最小路径和
+ */
+public class MinPathSum {
+
+    /**
+     * 空间复杂度O(mn)，比较不推荐的做法
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    dp[i][j] = 10000;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i + 1 < m && j + 1 < n) {
+                    dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + grid[i + 1][j]);
+                    dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + grid[i][j + 1]);
+                } else if (i + 1 < m) {
+                    dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + grid[i + 1][j]);
+                } else if (j + 1 < n) {
+                    dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + grid[i][j + 1]);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 不修改原数组条件下空间优化
+     * @param grid
+     * @return
+     */
+    public int minPathSum2(int[][] grid) {
+        int len = grid[0].length;
+        int[] dp = new int[len];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < len; i++)
+            dp[i]=dp[i-1]+grid[0][i];
+        for (int i = 1; i < grid.length; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for (int j = 1; j < len; j++)
+                dp[j] = Math.min(dp[j-1]+grid[i][j], dp[j]+grid[i][j]);
+        }
+        return dp[len-1];
+    }
+}
