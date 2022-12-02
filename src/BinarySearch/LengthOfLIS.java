becc89d232bfe46c9f6318872c84372a9b732a13
3ilence <silence.zhu.cn@gmail.com>
2022-12-02 22:31:41 +0800

    300.lengthOfLIS : DynamicProgramming+BinarySearch, this is hard for me to thind out

--- src/BinarySearch/LengthOfLIS.java
+++ src/BinarySearch/LengthOfLIS.java
@@ -1,4 +1,4 @@
-package GreedyAlgorithm;
+package BinarySearch;
 
 import java.util.Arrays;
 
@@ -12,20 +12,23 @@ public class LengthOfLIS {
 
     /**
      * 想过单调栈做，但是单调栈不能解决"最长"
+     * 动态规划+二分，这种做法我觉得我做几次都不会想到
+     * tails[k] 的值代表 长度为 k+1子序列 的尾部元素值。
      * @param nums
      * @return
      */
     public int lengthOfLIS(int[] nums) {
-        int res = 1;
-        int[] dp = new int[nums.length];//以i结尾的LIS
-        Arrays.fill(dp, 1);
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+        int[] tails = new int[nums.length];
+        int res = 0;
+        for(int num : nums) {
+            int i = 0, j = res;
+            while(i < j) {
+                int m = (i + j) / 2;
+                if(tails[m] < num) i = m + 1;
+                else j = m;
             }
-            res = Math.max(res, dp[i]);
+            tails[i] = num;
+            if(res == j) res++;
         }
         return res;
     }
