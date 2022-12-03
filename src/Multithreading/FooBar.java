6164dcc676de5ab48228fa5d6d0fbd3786a6110b
3ilence <silence.zhu.cn@gmail.com>
2022-12-03 10:21:26 +0800

    1115.FooBar : Semaphore, also there is also many other methods like BlockedQueue, ReentrantLock,etc.

---
+++ src/Multithreading/FooBar.java
@@ -0,0 +1,39 @@
+package Multithreading;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @ClassName : FooBar
+ * @Author : Silence
+ * @Date: 2022/12/3 9:57
+ * @Description : 1115. 交替打印 FooBar
+ */
+public class FooBar {
+    private int n;
+    Semaphore s1 = new Semaphore(1);
+    Semaphore s2 = new Semaphore(0);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            s1.acquire();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            s2.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            s2.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            s1.release();
+        }
+    }
+}
