fe3f3d533d349b14b38abf5fa308398f6a3d0c79
3ilence <silence.zhu.cn@gmail.com>
2022-12-06 20:58:37 +0800

    392.isSubsequence : DynamicProgramming, similar to 115.numDistinct and 72.minDistance

--- src/DynamicProgramming/IsSubsequence.java
+++ src/DynamicProgramming/IsSubsequence.java
@@ -8,6 +8,42 @@ package DynamicProgramming;
  */
 public class IsSubsequence {
 
+    /**
+     * dp[i][j]表示s.substring(0, i)是否是t.substring(0, j)的子序列
+     * 其实可以优化，因为dp[0][j] = true是恒成立，这样代码更精简，实践：6ms->4ms
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence2(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m > n) {
+            return false;
+        }
+        // 需要注意的点是""是所有串的子串
+        if (m == 0) {
+            return true;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];//dp[i][j]表示s.substring(0, i)是否是t.substring(0, j)的子序列
+        for (int i = 1; i <= m; i++) {
+            for (int j = i; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    if (i == 1) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+                if (i == m && dp[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     // <https://leetcode-cn.com/problems/is-subsequence/>
     /**
      * 可以当成是最长公共子序列来做，如果最长公共子序列长度等于s.length()，说明s为t的子序列
