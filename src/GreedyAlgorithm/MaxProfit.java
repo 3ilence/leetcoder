d0a7438689a102450b834b850d35511942ae0ebd
3ilence <silence.zhu.cn@gmail.com>
2022-12-06 22:53:11 +0800

    122.maxProfit : GreedyAlgorithm

--- src/GreedyAlgorithm/MaxProfit.java
+++ src/GreedyAlgorithm/MaxProfit.java
@@ -8,6 +8,22 @@ package GreedyAlgorithm;
  */
 public class MaxProfit {
     /**
+     * 很好理解，我可以在第1天到第length-1天卖股票，但是我在卖之前需要先买股票，如果在卖股票时获取最大收益？当然是在最低点买股票
+     * 故需要动态记录当天之前的股票最低价pre，那么prices[i] - pre就是今天卖掉股票可以获得的最大利润
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        int pre = prices[0];//代表当前日子之前最低的股票价格
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            res = Math.max(prices[i] - pre, res);
+            pre = Math.min(pre, prices[i]);
+        }
+        return res;
+    }
+
+    /**
      * 贪心
      * @param prices 价格
      * @return 最大利润
