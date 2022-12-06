2fec874a145546e60f5e8bb8442756ae37bd165b
3ilence <silence.zhu.cn@gmail.com>
2022-12-06 14:34:55 +0800

    583.minDistance : DynamicProgramming

--- src/DynamicProgramming/MinDistance.java
+++ src/DynamicProgramming/MinDistance.java
@@ -6,11 +6,38 @@ import java.util.Arrays;
  * @ClassName : MinDistance
  * @Author : Silence
  * @Date: 2022/11/25 15:09
- * @Description : 72. 编辑距离
+ * @Description : 72. 编辑距离 && 583. 两个字符串的删除操作
  */
 public class MinDistance {
 
     /**
+     * 583. 两个字符串的删除操作，和编辑距离的区别在于只有删除操作，没有替换操作，基本思路是一样的
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance2(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i * j == 0) {
+                    dp[i][j] = i + j;
+                    continue;
+                }
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]) + 1);
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * 72. 编辑距离
      * 这是我看了dp数组定义后做的答案，在用例(sea,eat)的时候答案出错，原因是se->e的这个状态计算出错，对应的dp[1][0]
      * 原因在于没有对s1[i] == s2[j]进行判断，没有进行判断也是因为0-1 = -1，所以这种dp数组定义还是有问题，应该声明为dp[m+1][n+1]
      * @param word1
