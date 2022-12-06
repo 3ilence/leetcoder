60b518e03e6536750dac0a2519c847c00522d2c3
3ilence <silence.zhu.cn@gmail.com>
2022-12-06 20:38:38 +0800

    72.minDistance

--- src/DynamicProgramming/MinDistance.java
+++ src/DynamicProgramming/MinDistance.java
@@ -1,7 +1,5 @@
 package DynamicProgramming;
 
-import java.util.Arrays;
-
 /**
  * @ClassName : MinDistance
  * @Author : Silence
@@ -9,6 +7,33 @@ import java.util.Arrays;
  * @Description : 72. 编辑距离 && 583. 两个字符串的删除操作
  */
 public class MinDistance {
+
+    /**
+     * 再次做编辑距离，大约10分钟搞定，果然动态规划主要还是dp数组的定义难想
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance3(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
 
     /**
      * 583. 两个字符串的删除操作，和编辑距离的区别在于只有删除操作，没有替换操作，基本思路是一样的
