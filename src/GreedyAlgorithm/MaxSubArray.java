eada67ed0f6e040c5e99aaa88f815e70f947ea96
3ilence <silence.zhu.cn@gmail.com>
2022-12-06 21:04:32 +0800

    53.maxSubArray : add comment

--- src/GreedyAlgorithm/MaxSubArray.java
+++ src/GreedyAlgorithm/MaxSubArray.java
@@ -7,6 +7,13 @@ package GreedyAlgorithm;
  * @Description : 53. 最大子数组和
  */
 public class MaxSubArray {
+
+    /**
+     * 其实现在想清楚了dp的空间优化这题的贪心做法也就不难想了，这题本质就是动规进行空间优化
+     * 我们每次状态转移的时候仅仅依赖上上一个连续和，也就是状态依赖链表长为1，也就没必要dp数组了
+     * @param nums
+     * @return
+     */
     public int MaxSubArray(int[] nums) {
         int res = nums[0] , sum = 0;
         for (int i : nums) {
