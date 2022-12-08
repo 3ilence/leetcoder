5810f05a2670d5e6e9b4700c3dae18f5867c818d
3ilence <silence.zhu.cn@gmail.com>
2022-12-08 18:18:46 +0800

    494.findTargetSumWays : BagQuestion

--- src/DynamicProgramming/FindTargetSumWays.java
+++ src/DynamicProgramming/FindTargetSumWays.java
@@ -9,6 +9,66 @@ package DynamicProgramming;
 public class FindTargetSumWays {
 
     /**
+     * 动规，空间优化
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays3(int[] nums, int target) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if ((sum + target) % 2 == 1 || (sum + target) < 0) {
+            return 0;
+        }
+        target = (sum + target) / 2;
+        int len = nums.length, res = 0;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= len; i++) {
+            for (int j = target; j >= 0; j--) {
+                if (j >= nums[i - 1]) {
+                    dp[j] = dp[j] + dp[j - nums[i - 1]];
+                } else {
+                    dp[j] = dp[j];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+    /**
+     * 动规，空间未优化
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays2(int[] nums, int target) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if ((sum + target) % 2 == 1 || (sum + target) < 0) {
+            return 0;
+        }
+        target = (sum + target) / 2;
+        int len = nums.length, res = 0;
+        int[][] dp = new int[len + 1][target + 1];//dp[i][j]表示在数组前i个元素中挑选，能使和为j的组合的种数
+        dp[0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[len][target];
+    }
+
+    /**
      * 01背包，空间未优化
      * @param nums nums
      * @param target target
