cbe15869a506596bbd4f93e8a2eec3834104d0a2
3ilence <silence.zhu.cn@gmail.com>
2022-12-08 17:39:06 +0800

    1049.lastStoneWeightII : BagQuestion, the key is to compress the state transition array

--- src/DynamicProgramming/LastStoneWeightII.java
+++ src/DynamicProgramming/LastStoneWeightII.java
@@ -9,6 +9,72 @@ package DynamicProgramming;
 public class LastStoneWeightII {
 
     /**
+     * 空间压缩后
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeightII4(int[] stones) {
+        //从这对石头里选出最接近sum/2的重量
+        int sum = 0;
+        for (int i : stones) {
+            sum += i;
+        }
+        int target = sum / 2;//如果不能整除会截断
+        int len = stones.length, res = 0;
+        boolean[] dp = new boolean[target + 1];//dp[i][j]代表前i个石头能否达到重量j
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            //注意空间压缩后需要倒序进行状态转移，如果正序的话dp[i-1][j - stones[i-1]]被dp[i][j - stones[i-1]]覆盖
+            //相当于把dp[i][j - stones[i-1]]当成dp[i-1][j - stones[i-1]]来用了
+            for (int j = target; j >= stones[i - 1]; j--) {
+                if (dp[j - stones[i - 1]]) {
+                    dp[j] = true;
+                }
+                if (dp[j]) {
+                    res = Math.max(res, j);
+                }
+            }
+        }
+        return res;
+        //return Math.abs(res * 2 - sum);
+    }
+
+    /**
+     * 空间压缩前
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeightII3(int[] stones) {
+        //从这对石头里选出最接近sum/2的重量
+        int sum = 0;
+        for (int i : stones) {
+            sum += i;
+        }
+        int target = sum / 2;//如果不能整除会截断
+        int len = stones.length, res = 0;
+        boolean[][] dp = new boolean[len + 1][target + 1];//dp[i][j]代表前i个石头能否达到重量j
+        for (int i = 0; i <= len; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j >= stones[i - 1]) {
+                    if (dp[i-1][j] || dp[i-1][j - stones[i - 1]]) {
+                        dp[i][j] = true;
+                    }
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+
+                if (dp[i][j]) {
+                    res = Math.max(res, j);
+                }
+            }
+        }
+        return Math.abs(res * 2 - sum);
+    }
+
+    /**
      * 动规。选取任意i个石头，使重量和尽量接近总重量一半
      * @param stones stones
      * @return res
@@ -56,6 +122,6 @@ public class LastStoneWeightII {
     }
 
     public static void main(String[] args) {
-        System.out.println(new LastStoneWeightII().lastStoneWeightII(new int[] {2,7,4,1,8,1}));
+        System.out.println(new LastStoneWeightII().lastStoneWeightII4(new int[] {31,26,33,21,40}));
     }
 }
