43135163e3b54240579d5055cd0282662a28329c
3ilence <silence.zhu.cn@gmail.com>
2022-12-11 22:46:13 +0800

    669.trimBST : Iteration

--- src/Tree/TrimBST.java
+++ src/Tree/TrimBST.java
@@ -10,6 +10,41 @@ import datastructure.Tree.TreeNode;
  */
 public class TrimBST {
 
+    /**
+     * 题解做法，迭代
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public TreeNode trimBST2(TreeNode root, int low, int high) {
+        while (root != null && (root.val < low || root.val > high)) {
+            if (root.val < low) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        if (root == null) {
+            return null;
+        }
+        for (TreeNode node = root; node.left != null; ) {
+            if (node.left.val < low) {
+                node.left = node.left.right;
+            } else {
+                node = node.left;
+            }
+        }
+        for (TreeNode node = root; node.right != null; ) {
+            if (node.right.val > high) {
+                node.right = node.right.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return root;
+    }
+
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) {
             return null;
